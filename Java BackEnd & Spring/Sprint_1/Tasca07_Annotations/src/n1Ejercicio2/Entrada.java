6b0e68369b44ce24490c601c8baa11b846b8286a
Jordi Hornos <146071125+Jodrix-15@users.noreply.github.com>
2023-10-25 16:33:25 +0200

    Add files via upload

--- Java BackEnd & Spring/Sprint_1/Tasca07_Annotations/src/n1Ejercicio2/Entrada.java
+++ Java BackEnd & Spring/Sprint_1/Tasca07_Annotations/src/n1Ejercicio2/Entrada.java
@@ -1,114 +1,68 @@
-package n1Ejercicio2;
+package n2Ejercicio1;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Entrada extends Exception{
-	/*Crea una classe anomenada "Entrada". Aquesta classe ha 
-	 * de servir per controlar les diferents excepcions que 
-	 * poden aparèixer en Java a l’hora d’introduir dades per 
-	 * teclat utilitzant la classe Scanner.
-	 * El primer que s’ha de fer és instanciar un objecte de la 
-	 * classe Scanner i a partir d’aquí, crear mètodes estàtics per 
-	 * llegir els diferents tipus de dades des del teclat
-	 */
+public class Entrada {
 	
-	/*Tots els mètodes reben un String amb el missatge que es vol 
-	 * mostrar a l’usuari/ària, per exemple: “Introdueix la teva edat”, 
-	 * i retornen la dada oportuna introduïda per l’usuari/ària en cada 
-	 * mètode, per exemple: un byte amb l’edat de l’usuari/ària.
-	 * Mètodes a implantar capturant l’excepció de la classe InputMismatchException:
-		public static byte llegirByte(String missatge)
-		public static int llegirInt(String missatge);
-		public static float llegirFloat(String missatge);
-		public static double llegirDouble(String missatge);
-		
-		Mètodes a implantar capturant l’excepció de la classe Exception:
-		public static char llegirChar(String missatge);
-		public static String llegirString(String missatge);
-		public static boolean llegirSiNo(String missatge), 
-		si l’usuari/ària introdueix “s”, retorna “true”, si 
-		l’usuari introdueix “n”, retorna “false”.
-	 */
-	public Scanner input() {
+	public static Scanner input() {
 
 		return new Scanner(System.in);
 	}
 	
-	public byte llegirByte(String msj) throws InputMismatchException {
+	public static byte llegirByte(String msj) throws InputMismatchException {
 		Object num;
 		byte numCorrecto;
 		System.out.print(msj);
 		
 		num = input().nextByte();
+		numCorrecto = (byte) num;
 
-		if(!(num instanceof Byte)) {
-			throw new InputMismatchException();
-		}else {
-			numCorrecto = (byte) num;
-		}
-		
-		
 		return numCorrecto;
 	}
 	
-	public int llegirInt(String msj) throws InputMismatchException{
+	public static int llegirInt(String msj) throws InputMismatchException{
 		Object obj;
 		int num;
 		
 		System.out.print(msj);
 		obj = input().nextInt();
-		
-		if(!(obj instanceof Integer)) {
-			throw new InputMismatchException();
-		}else {
-			num = (int) obj;
-		}
+		num = (int) obj;
 
 		return num;
 	}
 	
-	public float llegirFloat(String msj) throws InputMismatchException{
+	public static float llegirFloat(String msj) throws InputMismatchException{
 		Object obj;
 		float num;
 		
 		System.out.print(msj);
-		obj = input().nextFloat();
-		
-		if(!(obj instanceof Float)) {
-			throw new InputMismatchException();
-		}else {
-			num = (float) obj;
-		}
+		obj = input().nextFloat();	
+		num = (float) obj;
 
 		return num;
 		
 	}
 	
-	public double llegirDouble(String msj) throws InputMismatchException{
+	public static double llegirDouble(String msj) throws InputMismatchException{
 		Object obj;
 		double num;
 		
 		System.out.print(msj);
 		obj = input().nextDouble();
-		
-		if(!(obj instanceof Double)) {
-			throw new InputMismatchException();
-		}else {
-			num = (double) obj;
-		}
+		num = (double) obj;
 
 		return num;
 	}
 	
-	public char llegirChar(String msj) throws Exception {
+	public static char llegirChar(String msj) throws Exception {
 		Object obj;
 		char ch;
 		
 		System.out.print(msj);
 		obj = input().next();
 		
-		if(!(obj instanceof String) || obj.toString().length() > 1) {
+		if(obj.toString().length() > 1) {
 			throw new Exception();
 		}else {
 			ch = obj.toString().charAt(0);
@@ -117,17 +71,24 @@ public class Entrada extends Exception{
 		return ch;
 	}
 	
-	public String llegirString(String msj) {
-		//No hace falta excepciones porque siempre va a devolver un String
-		System.out.print(msj);
-		String texto = input().nextLine();
+	public static String llegirString(String msj) {
+		
+		String texto;
+	
+		do {
+			System.out.print(msj);
+			texto = input().nextLine();
+			
+			if(texto.matches(".*\\d+.*")) {
+				System.out.println("No debe contener numeros");
+			}
+		}while(texto.matches(".*\\d+.*"));
 
 		return texto;
 	}
 	
-	public boolean llegirSiNo(String msj) throws Exception{
-		/*si l’usuari/ària introdueix “s”, retorna “true”, si 
-		l’usuari introdueix “n”, retorna “false”.*/
+	public static boolean llegirSiNo(String msj) throws Exception{
+	
 		
 		Object obj;
 		char ch;
@@ -137,8 +98,7 @@ public class Entrada extends Exception{
 		obj = input().next();
 		
 		
-		if(!(obj instanceof String) || 
-				obj.toString().length() > 1 || 
+		if(obj.toString().length() > 1 || 
 				((!(obj.toString().equalsIgnoreCase("s")) && !(obj.toString().equalsIgnoreCase("n"))))) {
 			throw new Exception();
 		}else {
